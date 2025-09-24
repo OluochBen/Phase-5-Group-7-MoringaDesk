2af5bacc0f1dcff2d000d28f927497ba94d3e079
Ben Oluoch <splashmaxsoln@gmail.com>
2025-09-24 21:31:04 +0300

    Created homepage

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import StatsSection from '../components/StatsSection'
+import TopicsGrid from '../components/TopicsGrid'
+import FeaturesGrid from '../components/FeaturesGrid'
+import Testimonials from '../components/Testimonials'
+import CallToAction from '../components/CallToAction'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <StatsSection />
+      <section className="py-16 bg-white">
+        <div className="container mx-auto px-6">
+          <TopicsGrid />
+        </div>
+      </section>
+      <section className="py-16 bg-gray-50">
+        <div className="container mx-auto px-6">
+          <FeaturesGrid />
+        </div>
+      </section>
+      <Testimonials />
+      <CallToAction />
+    </>
+  )
+}
